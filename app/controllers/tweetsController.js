2247d9f0d0b112876eb2b65311fc94ac1533de77
pravingoswami <goswami4737@gmail.com>
2020-03-14 11:14:40 +0530

    sample for search how to get tweets by query

--- app/controllers/tweetsController.js
+++ app/controllers/tweetsController.js
@@ -1,3 +1,18 @@
+const Twitter = require('twitter')
+
 module.exports.tweetsList = (req, res) => {
-    res.json('welcome')
-}
+    const search = req.query.search
+
+    const client = new Twitter({
+        consumer_key: '881flbXb16SbwdP3R2sRKvdji',
+        consumer_secret: '1lgXzKUdWoMzdvba4r1YEY7gAEXqlMOSRMq99Iz7mUM2nNikFi',
+        access_token_key: '1132229170388332545-j5aLvteIDWWuSyHyJPvqXgsHi3DMpP',
+        access_token_secret: 'ISSVhx8UxSnDZ7cX1TklJ5BfYHIbZrGi2hJa8Sb97G2i7'
+    })
+
+    // ref - https://www.npmjs.com/package/twitter
+
+    client.get('search/tweets', {q : search})
+        .then(tweets => res.json(tweets) )
+        .catch(err => res.json(err))
+}   
